507aac1d609bdb54779ef1cf6348718e148d8cdb
Armando <jarodriguezvera@gmail.com>
2022-05-06 08:35:20 -0500

    Manejo de lectura y escritura en Bluetooth

--- app/src/main/java/com/temas/selectos/bluetootharduino/MainActivity.java
+++ app/src/main/java/com/temas/selectos/bluetootharduino/MainActivity.java
@@ -1,5 +1,6 @@
 package com.temas.selectos.bluetootharduino;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.bluetooth.BluetoothAdapter;
@@ -7,11 +8,14 @@ import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.util.Log;
 import android.view.View;
 import android.widget.CompoundButton;
 import android.widget.RadioGroup;
 import android.widget.Switch;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.IOException;
@@ -21,21 +25,45 @@ import java.util.UUID;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    private final int handlerState=0;
     private String direccion;
     private BluetoothAdapter btAdaptador;
     private BluetoothSocket btSocket=null;
     private static final UUID btUUID=UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
     //private static final UUID btUUID=UUID.randomUUID();
-    final int handlerState=0;
     private Handler hBTcomunica;
     private Switch swPrendeApaga;
     private ComunicaThread comunicaThreadBT;
+    private TextView txtvDato;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         swPrendeApaga= findViewById(R.id.swPrendeApaga);
         btAdaptador= BluetoothAdapter.getDefaultAdapter();
+        txtvDato= findViewById(R.id.txtvDato);
+
+        hBTcomunica = new Handler(Looper.getMainLooper()){
+            @Override
+            public void handleMessage(Message msg) {
+                Log.d("Mensaje","Entrando al manejador");
+                if( msg.what == handlerState)
+                {
+                    Log.d("Menwhat","Coinciden los mensajes");
+                    char Caracter= (char)msg.obj;
+                    if(Caracter == 'C'){
+                        txtvDato.setText("Se está enviando una C");
+                    }
+                    if( Caracter == 'D'){
+                        txtvDato.setText("Se está enviando una D");
+                    }
+                }
+            }
+
+        };
+
+
 
         swPrendeApaga.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
@@ -45,13 +73,14 @@ public class MainActivity extends AppCompatActivity {
                     Toast.makeText(getApplicationContext(),"Prendido",Toast.LENGTH_SHORT).show();
                 }
                 else {
-                    comunicaThreadBT.write("A");
+                    comunicaThreadBT.write("B");
                     Toast.makeText(getApplicationContext(),"Apagado",Toast.LENGTH_SHORT).show();
                 }
             }
         });
 
     }
+
 
     @Override
     protected void onResume() {
@@ -84,7 +113,11 @@ public class MainActivity extends AppCompatActivity {
         }
 
         comunicaThreadBT= new ComunicaThread(btSocket);
+       // comunicaThreadBT.run();
         comunicaThreadBT.start();
+
+
+
     }
 
     @Override
@@ -104,24 +137,33 @@ public class MainActivity extends AppCompatActivity {
 
 
         private ComunicaThread(BluetoothSocket socket) {
+            InputStream tmpIn= null;
+            OutputStream tmpOut= null;
+
             try {
-                this.minputStream= socket.getInputStream();
-                this.moutputStream= socket.getOutputStream();
+
+                tmpIn= socket.getInputStream();
+                tmpOut= socket.getOutputStream();
             }
             catch (IOException e)
             {
                 Log.d("eSocket","Error: " +e.toString());
             }
+            this.minputStream= tmpIn;
+            this.moutputStream= tmpOut;
         }
-/*
+
         public void run(){
-            byte[] buffer= new byte[256];
-            int bytes=0;
+
+            byte[] buffer= new byte[1];
             while (true){
+                Log.d("Run","Método que lee la entrada del puerto serie "  );
                 try {
-                    bytes=minputStream.read(buffer);
-                    String LMensaje= new String(buffer,0,bytes);
-                    hBTcomunica.obtainMessage(handlerState,bytes,-1,LMensaje).sendToTarget();
+                    minputStream.read(buffer);
+                    char ch=(char)buffer[0];
+                    Log.d("Run","Método que lee la entrada del puerto serie: " +ch );
+
+                    hBTcomunica.obtainMessage(handlerState,ch).sendToTarget();
                 } catch (IOException e) {
                     e.printStackTrace();
                     break;
@@ -129,7 +171,7 @@ public class MainActivity extends AppCompatActivity {
             }
 
         }
-*/
+
         public void write(String input)  {
 
             try {
@@ -139,6 +181,8 @@ public class MainActivity extends AppCompatActivity {
                // finish();
             }
         }
+
+
     }
 
 }
